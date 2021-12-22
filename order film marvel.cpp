c04596cfc0cce2140ec88c4a7e2d0ad9e1c6f22a
Rivaldo <92324602+Rivaldo2117051054@users.noreply.github.com>
2021-12-22 08:53:35 +0700

    Update order film marvel.cpp

--- order film marvel.cpp
+++ order film marvel.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 string marvel,namaku;   
 char back;   
-int harga[5]{98000, 30000, 35000, 90000, 55000},bulan,bayar,kurang,kembali,nama,z=0;
+int harga[5]{98000, 30000, 35000, 90000, 55000},bulan,bayar,kurang,kembali,nama,z=0; 
 
 void title(){ 
 	string judul="==================== Marvel Film store ============";  
